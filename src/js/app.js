55e0418d0b79c8ae501ae3ec44c1a51c06083997
alfred <alfmohenu@gmail.com>
2019-02-25 17:40:52 +0000

    Initial animation styling added. Jquery and Velocity added.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,48 @@
+var $ = require('jquery');
+window.jQuery = window.$ = $;
+require('velocity-animate');
+
+import * as home from "./homepage-desc";
+
 if (module.hot) {
   module.hot.accept();
 }
+
+onMouseOverAnimate(".nav-items__item-about", home.about);
+onMouseOutAnimate(".nav-items__item-about");
+
+onMouseOverAnimate(".nav-items__item-work", home.work);
+onMouseOutAnimate(".nav-items__item-work");
+
+onMouseOverAnimate(".nav-items__item-contact", home.contact);
+onMouseOutAnimate(".nav-items__item-contact");
+
+
+/* Helper functions */
+
+function onMouseOverAnimate(ele, content) {
+  $(ele).on("mouseover", () => {
+    $(".home-desc").html(content)
+    animateDesc(".home-desc");
+  })
+}
+
+function onMouseOutAnimate(ele) {
+  $(ele).on("mouseout", () => {
+    deAnimateDesc(".home-desc");
+  })
+}
+
+function deAnimateDesc(ele) {
+  $(ele).removeClass("home-desc__visible")
+}
+
+function animateDesc(ele) {
+  $(ele).velocity({
+    opacity: 1,
+  }, {
+      duration: 500,
+    });
+  $(ele).addClass("home-desc__visible")
+}
+
---
+++ src/js/homepage-desc.js
@@ -0,0 +1,7 @@
+const home = {
+    about: "We bring strategy, design and development together to create amazing experiences.",
+    work: "Work",
+    contact:"Contact"
+}
+
+module.exports = home;
