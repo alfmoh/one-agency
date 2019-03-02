0d5380ce68427193730b0d2af0247529052ce8b7
alfred <alfmohenu@gmail.com>
2019-03-02 06:06:57 +0000

    Logo animation intiated. Desc font updated. Velocity ui pack added.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 var $ = require('jquery');
 window.jQuery = window.$ = $;
 require('velocity-animate');
+require('velocity-ui-pack');
 
 let lastDescElement;
 
@@ -9,31 +10,61 @@ if (module.hot) {
 }
 
 onMouseOverAnimate(".nav-items__item-about", ".home-desc__about");
-onMouseOutAnimate(".nav-items__item-about",".home-desc__about");
+onMouseOutAnimate(".nav-items__item-about", ".home-desc__about");
 
 onMouseOverAnimate(".nav-items__item-work", ".home-desc__work");
-onMouseOutAnimate(".nav-items__item-work",".home-desc__work");
+onMouseOutAnimate(".nav-items__item-work", ".home-desc__work");
 
 onMouseOverAnimate(".nav-items__item-contact", ".home-desc__contact");
-onMouseOutAnimate(".nav-items__item-contact",".home-desc__contact");
+onMouseOutAnimate(".nav-items__item-contact", ".home-desc__contact");
 
 
 function onMouseOverAnimate(ele, descItem) {
   $(ele).on("mouseover", () => {
-    if(lastDescElement)
+    if (lastDescElement)
       $(lastDescElement).addClass("content--hidden");
-    $(".home-desc").append($(descItem))
-    $(descItem).removeClass("content--hidden")
+    $(".home-desc").append($(descItem));
 
-    animateDesc(".home-desc");
+    $(descItem).removeClass("content--hidden");
+
+    animateDesc(descItem);
   })
 }
 
-function onMouseOutAnimate(ele,descItem) {
+function onMouseOutAnimate(ele, descItem) {
   $(ele).on("mouseout", () => {
     lastDescElement = descItem;
   })
 }
+
+const navLogo = $(".nav-logo__logo");
+const navDot = $(".nav-logo__dot");
+
+var loadingSequence = [
+  {
+    e: navLogo, p: { translateX: "-15px" }, 
+    o: {
+      duration: 800
+    }
+  },
+  {
+    e: navDot, p: { translateY: "-15px" }, 
+    o: {
+      duration: 800
+    }
+  },
+]
+
+
+
+$(navLogo).on("mouseover", () => {
+  $.Velocity.RunSequence(loadingSequence);
+})
+
+$(navLogo).on("mouseout", () => {
+  $(navLogo).velocity("reverse");
+  $(navDot).velocity("reverse");
+})
 
 
 function animateDesc(ele) {
